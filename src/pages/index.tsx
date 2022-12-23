d98bf05d6ac9aabaeaa438be8fb9927de2089341
Rino Tovar <rinotova@gmail.com>
2022-12-23 15:58:27 +0100

    Packages update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import { type NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return "Hello World";
+  return (
+    <>
+      <Head>
+        <title>Woof</title>
+        <meta name="description" content="As seen in The Office" />
+      </Head>
+      <div></div>
+    </>
+  );
 };
 
 export default Home;
