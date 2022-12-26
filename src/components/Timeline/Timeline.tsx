0290f253ea79ed5812f0079338a3451b05f0375d
Rino Tovar <rinotova@gmail.com>
2022-12-26 18:34:54 +0100

    test2

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -3,12 +3,16 @@ import CreateWoofForm from "../CreateWoofForm/CreateWoofForm";
 import Woof from "../Woof/Woof";
 
 const Timeline = () => {
-  const { data, isLoading, isError } = trpc.woof.list.useQuery({});
+  const { data, isLoading, isError, isFetching, isStale } =
+    trpc.woof.list.useQuery({});
 
   return (
     <>
       <CreateWoofForm />
       {"Is Loading=" + isLoading}
+      {"Is Fetching=" + isFetching}
+      {"Is Stale=" + isStale}
+      {"Data=" + JSON.stringify(data)}
       {!isLoading && !isError && (
         <div className="mx-auto max-w-4xl p-4">
           {data.wooves.map((woof) => {
